61c8946b8d12cafe5ad588e2dc2d43486579cf3b
leosilvaq <132711984+leosilvaq@users.noreply.github.com>
2024-10-28 16:55:36 -0300

    Update users.js
    Att de rotas para usuarios

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,39 +1,66 @@
-import express from 'express';
+import express from 'express'
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
-import { loginValidation, newUserValidation } from '../middlewares/validation-user'
-const router = express.Router();
 
+import { validateUserRegistration } from './../middlewares/validation'
 
-export const users = [];
+const router = express.Router()
 
-router.post('/signup', newUserValidation, async (req, res) => {
-  const { name, email, password } = req.body;
+// Exportação nomeada
+export const users = []
 
-  const hashedPass = await bcrypt.hash(password, 10)
+router.post('/signup', validateUserRegistration, async (request, response) => {
+  const { name, email, password } = request.body
+
+  const emailAlreadyRegistration = users.find(user => user.email === email)
+
+  if (emailAlreadyRegistration) {
+    return response.status(400).json({
+      message: 'E-mail já cadastrado.'
+    })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   const newUser = {
     id: uuidv4(),
     name,
     email,
-    password: hashedPass
-  };
+    password: hashedPassword // salva a senha "encriptada" no array
+  }
 
-  users.push(newUser);
+  users.push(newUser)
 
-  return res.status(201).json({ message: `Seja bem vindo ${name}! Usuário registrado com sucesso!` });
-});
+  response.status(201).json({
+    message: 'Conta criada com sucesso.',
+    user: newUser
+  })
+})
 
-router.post('/login', loginValidation, (req, res) => {
-    const { user } = req
+router.post('/login', async (request, response) => {
+  const { email, password } = request.body
 
-  return res.status(200).json({ 
-    message: `Seja bem-vindo ${user.name}! Pessoa usuária logada com sucesso!`,
-    userId: user.email
-  });
+  const user = users.find(user => user.email === email)
 
-   
-});
+  if (!user) {
+    return response.status(404).json({
+      message: 'Usuário não encontrado.'
+    })
+  }
 
+  const passwordMatch = await bcrypt.compare(password, user.password)
 
+  if (!passwordMatch) {
+    return response.status(400).json({
+      message: "Credenciais inválidas."
+    })
+  }
+
+  response.status(200).json({
+    message: 'Login feito com sucesso!',
+    userId: user.id
+  })
+})
+
+// Exportação default
 export default router
