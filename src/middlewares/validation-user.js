2cfe6bb1b8df2767ec15ca7f206f938aa1863a26
leosilvaq <132711984+leosilvaq@users.noreply.github.com>
2024-10-28 16:58:17 -0300

    Update validation-user.js
    test att

--- src/middlewares/validation-user.js
+++ src/middlewares/validation-user.js
@@ -1,39 +1,39 @@
-import { users } from '../routes/users'
+// import { users } from '../routes/users'
 
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
 
-export const newUserValidation = (req, res, next) => {
+// export const newUserValidation = (req, res, next) => {
 
-    const { name, email, password } = req.body;
+//     const { name, email, password } = req.body;
 
-    if (!name) return res.status(400).json({ message: 'Por favor, verifique se passou o nome.' });
-    if (!email) return res.status(400).json({ message: 'Por favor, verifique se passou o email.' });
-    if (!password) return res.status(400).json({ message: 'Por favor, verifique se passou a senha.' });
+//     if (!name) return res.status(400).json({ message: 'Por favor, verifique se passou o nome.' });
+//     if (!email) return res.status(400).json({ message: 'Por favor, verifique se passou o email.' });
+//     if (!password) return res.status(400).json({ message: 'Por favor, verifique se passou a senha.' });
 
-    const userExists = users.find(user => user.email === email);
-    if (userExists) return res.status(400).json({ message: 'Email já cadastrado, insira outro.' });
+//     const userExists = users.find(user => user.email === email);
+//     if (userExists) return res.status(400).json({ message: 'Email já cadastrado, insira outro.' });
 
-    next();
-}
+//     next();
+// }
 
-export const loginValidation = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = users.find(user => user.email === email);
+// export const loginValidation = async (req, res, next) => {
+//     const { email, password } = req.body;
+//     const user = users.find(user => user.email === email);
 
-    if (!email) return res.status(400).json({ message: 'Insira um e-mail válido' });
-    if (!password) return res.status(400).json({ message: 'Insira uma senha válida' });
+//     if (!email) return res.status(400).json({ message: 'Insira um e-mail válido' });
+//     if (!password) return res.status(400).json({ message: 'Insira uma senha válida' });
 
 
-    if (!user) return res.status(404).json({ message: 'Email não encontrado no sistema, verifique ou crie uma conta' });
+//     if (!user) return res.status(404).json({ message: 'Email não encontrado no sistema, verifique ou crie uma conta' });
 
-    const isMatch = await bcrypt.compare(password, user.password);
+//     const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        return res.status(400).json({ message: 'usuario ou senha incorretos.' });
-    }
+//     if (!isMatch) {
+//         return res.status(400).json({ message: 'usuario ou senha incorretos.' });
+//     }
 
-    req.user = user
+//     req.user = user
 
-    next();
-}
+//     next();
+// }
 
