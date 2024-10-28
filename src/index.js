823c2061c07c5021d30b39b00255daf6dba0cff1
leosilvaq <132711984+leosilvaq@users.noreply.github.com>
2024-10-28 16:56:41 -0300

    Update index.js
    att API

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import express from 'express';
-import usersRouter from './routes/users';
-import notesRouter from './routes/notes';
-import cors from 'cors'; 
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+import usersRouter from './routes/users'
+import notesRouter from './routes/notes'
 
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
 app.use(cors())
-app.use(express.json());
-app.use('/users', usersRouter);
-app.use('/notes', notesRouter);
 
+app.use('/users', usersRouter)
+app.use('/notes', notesRouter)
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Bem vindo à aplicação' });
-});
-
-const port = 3333;
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
